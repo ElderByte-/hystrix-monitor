fb923259cd9e3e84826ea89dd542e267a1c7d06e
IsNull <p.buettiker@elderbyte.com>
2016-11-07 14:26:07 +0100

    enable hystrix dashboard

--- src/main/java/com/elderbyte/eureka/HystrixTurbineServer.java
+++ src/main/java/com/elderbyte/eureka/HystrixTurbineServer.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
@@ -18,6 +19,7 @@ import java.net.UnknownHostException;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableTurbine
+@EnableHystrixDashboard
 public class HystrixTurbineServer {
 
     private static final Logger log = LoggerFactory.getLogger(HystrixTurbineServer.class);
